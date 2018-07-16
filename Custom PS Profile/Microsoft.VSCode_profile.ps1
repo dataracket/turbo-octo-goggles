a0a0a701ca0340cea8257093adce9ed30c93be5c
Gust <AGust@clac.ca>
2018-07-16 09:57:32 -0700

    Updated script to not clear the host every time you bring up  anew prompt... -.-

--- Custom PS Profile/Microsoft.VSCode_profile.ps1
+++ Custom PS Profile/Microsoft.VSCode_profile.ps1
@@ -1,12 +1,11 @@
 Set-Location C:\
+$host.UI.RawUI.WindowTitle = Get-Location
+$Host.UI.RawUI.ForegroundColor = "white"
+$Host.UI.RawUI.BackgroundColor = "black"
 Clear-Host
 Write-Host "Beast mode active. Go get 'em, tiger."
 function Get-Time { return $(get-date | ForEach-Object { $_.ToLongTimeString() } ) }
 function prompt {
-    $host.UI.RawUI.WindowTitle = Get-Location
-    $Host.UI.RawUI.ForegroundColor = "white"
-    $Host.UI.RawUI.BackgroundColor = "black"
-    Clear-Host
     # Write the time
     write-host "[" -noNewLine
     write-host $(Get-Time) -foreground yellow -noNewLine
