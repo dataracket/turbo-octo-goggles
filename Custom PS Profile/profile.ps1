760b886e3b773277926f85867aec9c82ed283bb1
Gust <AGust@clac.ca>
2018-07-30 17:00:41 -0700

    More updates to profile script

--- Custom PS Profile/profile.ps1
+++ Custom PS Profile/profile.ps1
@@ -25,27 +25,13 @@ function PrintMenu {
     Write-Host("")
     Write-Host("Command             Function")
     Write-Host("-------             --------")
+    Write-Host("choco               Activates Choco")
     Write-Host -ForegroundColor Red ("cl                  Clear Shell and Reprint Command Menu")
     Write-Host -ForegroundColor Red ("CLACMenu            Retrieve CLAC custom cmdlets")
-    Write-Host("CheckProcess        Retrieve System Process Information")
-    Write-Host("CrossCertRm         Remove Inoperable Certificates")
-    Write-Host("GPR                 Group Policy (Remote)")
     Write-Host("HuntUser            Query SCCM For Last System Logged On By Specified User")
-    Write-Host("InstallApplication  Silent Install EXE, MSI, or MSP files")
-    Write-Host("JavaCache           Clear Java Cache")
     Write-Host("LastBoot            Get Last Reboot Time")
-    Write-Host("NetMSG              On-screen Message For Specified Workstation(s)")
-    Write-Host("NewADuser           Create New Active Directory Users From SAAR Forms")
-    Write-Host("Nithins             Opens Nithin's SCCM Client Tool")
     Write-Host("RDP                 Remote Desktop")
-    Write-Host("REARMOffice         Rearm Office 2013 Activation")
-    Write-Host("REARMWindows        Rearm Windows 7 OS Activation")
-    Write-Host("Reboot              Force Restart")
-    Write-Host("RmPrint             Clear Printer Drivers")
     Write-Host("RmUserProf          Clear User Profiles")
-    Write-Host("SCCM                Active Directory/SCCM Console")
-    Write-Host("SWcheck             Check Installed Software")
-    Write-Host("SYS                 All Remote System Info")
     Write-Host("UpdateProfile       Update PowerShell Profile (Will Overwrite Current Version & Any Changes)")
     Write-Host("")
     Write-Host("")
@@ -184,11 +170,241 @@ function UpdateProfile {
 
 }#End UpdateProfile
 
+function RDP {
+    <#
+.SYNOPSIS
+    Remote Desktop Protocol to specified workstation(s)
+
+.EXAMPLE
+    RDP Computer123456
+
+.EXAMPLE
+    RDP 123456
+#>
+
+    param(
+
+        [Parameter(Mandatory = $true)]
+        [String]$Computername
+    )
+
+    #Start Remote Desktop Protocol on specifed workstation
+    & "C:\windows\system32\mstsc.exe" /v:$computername /fullscreen
+}#End RDP
+
+function LastBoot {
+    <#
+.SYNOPSIS
+    Retrieve last restart time for specified workstation(s)
+
+.EXAMPLE
+    LastBoot Computer123456
+
+.EXAMPLE
+    LastBoot 123456
+#>
+
+    param(
+
+        [Parameter(Mandatory = $true)]
+        [String[]]$ComputerName,
+
+        $i = 0,
+        $j = 0
+    )
+
+    foreach ($Computer in $ComputerName) {
+
+        $computerOS = Get-WmiObject Win32_OperatingSystem -Computer $Computer
+
+        [pscustomobject] @{
+
+            ComputerName = $Computer
+            LastReboot   = $computerOS.ConvertToDateTime($computerOS.LastBootUpTime)
+        }
+    }
+}#End LastBoot
+
+<#
+	https://github.com/kjerk/shelljump
+
+	Commands (aliased):
+		jump (name)     | Jump to a previously bookmarked folder.
+		jumps or marks  | List all existing bookmarks.
+		mark (name)     | Bookmarks current directory as a given name.
+		unmark (name)   | Removes named bookmark.
+
+	Example:
+		C:\> cd '.\Program Files\Sublime Text 2'
+		C:\Program Files\Sublime Text 2> mark st2
+		C:\Program Files\Sublime Text 2> cd \
+		C:\> jump st2
+		C:\Program Files\Sublime Text 2> marks
+		st2        -> C:\Program Files\Sublime Text 2
+		C:\Program Files\Sublime Text 2> unmark st2
+#>
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions") | Out-Null
+
+function New-Bookmark {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 1)]
+        [String]$newName
+    )
+
+    if ( Check-BookmarksTimestamp -eq $true ) {
+        if (-not $global:bookmarks.ContainsKey($newName)) {
+            if ($global:bookmarks.ContainsValue($PWD.ToString())) {
+                "Warning: current path already bookmarked under different key."
+            }
+
+            $global:bookmarks.Add($newName, $PWD.ToString());
+
+            Write-Bookmarks
+        }
+        else {
+            "Key '$newName' already exists in bookmarks.";
+        }
+    }
+    else {
+        "Bookmarks on disk newer than loaded bookmarks. (Desynced)"
+        "Please refresh them with Read-Bookmarks, or overwrite with Write-Bookmarks."
+    }
+}
+
+function Remove-Bookmark {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 1)]
+        [String]$name
+    )
+
+    if ( Check-BookmarksTimestamp -eq $true ) {
+        if ($global:bookmarks.ContainsKey($name)) {
+            $global:bookmarks.Remove($name)  | Out-Null;
+            Write-Bookmarks
+        }
+        else {
+            "No key with value '$name' found."
+        }
+    }
+    else {
+        "Bookmarks on disk newer than loaded bookmarks. (Desynced)"
+        "Please refresh them with Read-Bookmarks, or overwrite with Write-Bookmarks."
+    }
+}
+
+function Read-Bookmarks {
+    $fPath = "$([System.IO.Path]::GetDirectoryName($profile))\Jumps.json";
+
+    if ([System.IO.File]::Exists($fPath)) {
+        $Global:JumpFileTime = (Get-Item "$([System.IO.Path]::GetDirectoryName($profile))\Jumps.json").LastWriteTime;
+        if (Test-Path $fPath) {
+            $global:bookmarks = @{};
+            $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+            $global:bookmarks = $ser.DeserializeObject((Get-Content $fPath))
+        }
+        else {
+            $global:bookmarks = @{};
+        }
+    }
+    else {
+        $global:bookmarks = @{};
+    }
+}
+
+function Check-BookmarksTimestamp {
+    $bPath = "$([System.IO.Path]::GetDirectoryName($profile))\Jumps.json"
+
+    if (Test-Path $bPath) {
+        $fDate = (Get-Item "$([System.IO.Path]::GetDirectoryName($profile))\Jumps.json").LastWriteTime;
+
+        if ($fDate -gt $Global:JumpFileTime) {
+            return $false;
+        }
+    }
+
+    return $true;
+}
+
+function Write-Bookmarks {
+    $profPath = [System.IO.Path]::GetDirectoryName($profile);
+    $newPath = "$profPath\Jumps.json";
+    $oldPath = "$profPath\Jumps.json.prev";
+
+    if (Test-Path $oldPath) {
+        del "$oldPath"
+    }
+
+    if (Test-Path $newPath) {
+        ren "$newPath" "$oldPath"
+    }
+
+    ConvertTo-Json $global:bookmarks | Out-File $newPath
+
+    $Global:JumpFileTime = (Get-Item $newPath).LastWriteTime;
+}
+
+function Invoke-Bookmark {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 1)]
+        [String]$jumpcat = '.'
+    )
+
+    if ($global:bookmarks.ContainsKey($jumpcat)) {
+        cd $global:bookmarks[$jumpcat].ToString();
+    }
+    else {
+        "Unknown jump target."
+        "Type: 'jumps' to get a list of available bookmarks."
+    }
+}
+
+function Get-Bookmarks {
+    $en = $global:bookmarks.GetEnumerator();
+
+    while ($en.MoveNext()) {
+        "$($en.Key.PadRight(10)) -> $($en.Value)"
+    }
+}
+
+Set-Alias jump   Invoke-Bookmark
+Set-Alias jumps  Get-Bookmarks
+Set-Alias marks  Get-Bookmarks
+Set-Alias mark   New-Bookmark
+Set-Alias unmark Remove-Bookmark
+
+#Load bookmarks from previous session.
+Read-Bookmarks
+
+# Tab Expansion override compatibility, comment out these two
+# blocks to leave tab completion alone.
+if (Test-Path Function:\TabExpansion) {
+    Rename-Item Function:\TabExpansion TabExpansionBackupJump
+}
+
+function TabExpansion([string] $line, [string] $lastword) {
+    if ( $line.StartsWith("jump ") -or $line.StartsWith("unmark ") ) {
+        return [string[]]$Global:bookmarks.Keys;
+    }
+    else {
+        # Fall back to the tab completion that was in place.
+        if (Test-Path Function:\TabExpansionBackupJump) {
+            TabExpansionBackupJump $line $lastword
+        }
+        else {
+            return $null;
+        }
+    }
+}
+
 ############### Setup Parameter autocomplete Hack #################
 
 <#
 What this script does
-1) Creates a scriptblock that gathers all AD information.  Garthered information is exported as a CSV in the temp directory
+1) Creates a scriptblock that gathers all AD information.  Gathered information is exported as a CSV in the temp directory
 2) Start the scriptblock under a new thread so powershell does not hang.
 3) Creates scriptblocks that imports the related CSV and created an autocomplete object.
 4) Modify powershell autocomplete to run custom scriptblock when parameter is detected.
